110b37e93971526df0de35f66e9149d144fd6268
David Rivas <159089517+Rivas17@users.noreply.github.com>
2025-11-18 11:47:49 -0500

    Update hospital.py

--- QT_Sistema_informacion/Model/hospital.py
+++ QT_Sistema_informacion/Model/hospital.py
@@ -16,11 +16,3 @@ class Hospital:
 
     def __str__(self):
         return f"Hospital: {self.nombre_hospital} ({len(self.lista_doctores)} Doctores)"
-
-#Ejemplo de uso (esto no va en el archivo, es solo para entender):
-# mi_hospital = Hospital("Hospital Central")
-# doc = Doctor(123,"David","Cirujano de rodilla")
-# mi_hospital.añadir_doctor("jorge")
-# mi_hospital.añadir_doctor(doc)
-# print(mi_hospital)
-# print(doc.informacion())
